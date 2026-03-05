f004d2b1c20c89c8cf94c20b650ce660cf9044c3
margarita <margarita.que@mail.ru>
2026-03-06 02:59:09 +0400

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/media/repository/PostRepository.java
+++ src/main/java/ru/job4j/media/repository/PostRepository.java
@@ -1,7 +1,19 @@
 package ru.job4j.media.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.job4j.media.model.Post;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByUserId (Long userId);
+
+    List<Post> findByCreatedAtBetween (LocalDateTime start, LocalDateTime end);
+
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
