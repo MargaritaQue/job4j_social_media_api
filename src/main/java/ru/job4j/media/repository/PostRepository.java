e85603203a35ba7706d64ad0a02099127f2c432f
margarita <margarita.que@mail.ru>
2026-03-06 18:59:32 +0400

    HQL, JPQL, Native SQL [#505064]

--- src/main/java/ru/job4j/media/repository/PostRepository.java
+++ src/main/java/ru/job4j/media/repository/PostRepository.java
@@ -3,6 +3,9 @@ package ru.job4j.media.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.media.model.Post;
 
 import java.time.LocalDateTime;
@@ -16,4 +19,35 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByCreatedAtBetween (LocalDateTime start, LocalDateTime end);
 
     Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            UPDATE Post post SET post.tittle = :tittle, post.content = :content
+            WHERE post.id = :id
+            """)
+    int postUpdateTittleAndContent(@Param("id") Long id, @Param("tittle") String tittle, @Param("content") String content);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            DELETE FROM PostImage pi WHERE pi.id = :imageId
+            """)
+    int postDeletePostImage(@Param("imageId") Long imageId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            DELETE FROM Post post WHERE post.id = :postId
+            """)
+    int postDelete(@Param("postId") Long postId);
+
+
+    @Query(value = """
+            SELECT post FROM Post post WHERE post.userId IN
+            (SELECT s.userFollowed FROM Subscription s WHERE s.userFollower = :userId) ORDER BY post.createdAt DESC
+            """,
+            countQuery = """
+            SELECT COUNT(post) FROM Post post WHERE post.userId IN
+            (SELECT s.userFollowed FROM Subscription s WHERE s.userFollower = :userId)
+            """
+    )
+    Page<Post> userPosts(@Param("userId") Long userId, Pageable pageable);
 }
--- src/main/java/ru/job4j/media/repository/UserRepository.java
+++ src/main/java/ru/job4j/media/repository/UserRepository.java
@@ -1,7 +1,31 @@
 package ru.job4j.media.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.media.model.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query("""
+            SELECT user FROM User AS user
+            WHERE user.username = :username AND user.password = :password
+            """)
+    Optional<User> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+
+    @Query("""
+            SELECT user FROM User user WHERE user.id IN
+            (SELECT sub.userFollower FROM Subscription sub WHERE sub.userFollowed = :userId)
+            """)
+    List<User> userSubscriptions(@Param("userId") Long userId);
+
+    @Query("""
+            SELECT user FROM User user WHERE user.id IN
+            (SELECT f.friendId FROM Friendship f WHERE f.userId = :userId) OR user.id IN
+            (SELECT f.userId FROM Friendship f WHERE f.friendId = :userId)
+            """)
+    List<User> userFriends(@Param("userId") Long userId);
 }
